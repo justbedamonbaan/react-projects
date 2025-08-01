3b927c8cddd321430fb6946fb27d4684e9d5a6da
justbedamon <damonbaan@hotmail.com>
2025-08-01 18:01:50 +0200

    tic tac toe is fully functional

--- src/tictactoe/Square.jsx
+++ src/tictactoe/Square.jsx
@@ -10,39 +10,79 @@ export function Square({ value, onSquareClick }) {
 }
 
 
-export function Board () {
+function Board () {
     const [squares, setSquares] = useState(Array(9).fill(null))
-
+    const [xIsNext, setXIsNext] = useState(true)
+    
     function handleClick(i) {
+        if (squares[i] || CalculateWinner(squares)){
+            return;
+        }
+
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        
+        if (xIsNext){
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
         setSquares(nextSquares)
-    }
+        setXIsNext(!xIsNext)
+    }   
+        const winner = CalculateWinner(squares)
+        let gameMessage
+        if (winner) {
+            gameMessage = "Winner: " + winner
+        }   else {
+            gameMessage = "Next player is: " + (xIsNext ? "X" : "O")
+        }
 
     return(
         <>
-        <p className="gameInfo">Next turn: </p>
-        <div className="game">
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}     />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}     />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}     />
-            </div>
+            <p className="gameInfo">{gameMessage}</p>
+            <div className="game">
+                <div className="board-row">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)}     />
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)}     />
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)}     />
+                </div>
 
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}     />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}     />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}     />
-            </div>
+                <div className="board-row">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)}     />
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)}     />
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)}     />
+                </div>
 
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}     />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}     />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}     />
+                <div className="board-row">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)}     />
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)}     />
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)}     />
+                </div>
             </div>
-        </div>
         </>
     )
+}
+
+function CalculateWinner(squares){
+    const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let i = 0; i < winningLines.length; i++) {
+        const [a, b, c] = winningLines[i]
+        
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+        }
+    }
+    return null;
 }
 
 export default Board;
--- src/tictactoe/TicTacToe.jsx
+++ src/tictactoe/TicTacToe.jsx
@@ -6,11 +6,6 @@ import Intro from '../components/Intro';
 function TicTacToePage () {
     return (
         <div className="container">
-            {/* <div className="pageIntro">
-                <h1 className='introTitle'>Tic Tac Toe</h1>
-                <div className="br"></div>
-                <p className="description">This is a tic tac toe game devloped with react and node.js</p>
-            </div > */}
             
             <Intro title="Tic Tac Toe" description="This is a tic tac toe game devloped with react and node.js"/>
 
